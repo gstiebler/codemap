5275931c34ce41a1fbedff85e263257004b6fe50
Guilherme Stiebler <guilhermemst@gmail.com>
2012-05-30 19:05:34 -0300

    Nó de grafo Output de erro

---
+++ src/gvpl/jdt/ErrorOutputter.java
@@ -0,0 +1,13 @@
+package gvpl.jdt;
+
+public class ErrorOutputter {
+	
+	static void warning(String error) {
+		System.out.println(error);
+	}
+
+	static void fatalError(String error) {
+		System.out.println(error);
+	}
+	
+}
