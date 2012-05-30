63be6a473ee87683959862b72e4cfb8b2b25d1d0
Guilherme Stiebler <guilhermemst@gmail.com>
2012-05-30 20:05:22 -0300

    Adicionando o graph builder

--- src/gvpl/jdt/Graph.java
+++ src/gvpl/jdt/Graph.java
@@ -4,55 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Graph {
-	
+
 	public enum NodeType {
-			E_INVALID_NODE_TYPE,
-            E_DIRECT_VALUE,
-            E_VARIABLE,
-            E_OPERATION,
-            E_FOR_LOOP
+		E_INVALID_NODE_TYPE,
+		E_DIRECT_VALUE,
+		E_VARIABLE,
+		E_OPERATION,
+		E_FOR_LOOP
 	}
-	
-	public class GraphNode
-	{
-		private GraphNodeId _id;
-		private String _name;
+
+	public class GraphNode {
+		public GraphNodeId _id;
+		public String _name;
 		private NodeType _type;
-		private List<GraphNode> _dependent_nodes;
+		List<GraphNode> _dependent_nodes;
 
 		public GraphNode(GraphNodeId id, String name, NodeType type) {
 			_id = id;
 			_name = name;
 			_type = type;
-	    }
+		}
 	};
-	
-	public class GraphNodeId
-	{
-		
+
+	public class GraphNodeId {
+
 		private int _id;
-		
-		public GraphNodeId(int id)
-		{
+
+		public GraphNodeId(int id) {
 			_id = id;
 		}
 	}
-	
-	
+
 	private List<GraphNode> _graph_nodes = new ArrayList<GraphNode>();
-	
-	public GraphNode addGraphNode(String name, NodeType type)
-	{
+
+	public GraphNode add_graph_node(String name, NodeType type) {
 		GraphNode graph_node = new GraphNode(new GraphNodeId(_graph_nodes.size()), name, type);
 		_graph_nodes.add(graph_node);
 		return graph_node;
 	}
 
-	public GraphNode findGraphNode(GraphNodeId id)
-	{
-	  if(id._id > _graph_nodes.size())
-	    ErrorOutputter.fatalError("Graph node id (" + id._id + ")not found.\n");
+	public GraphNode find_graph_node(GraphNodeId id) {
+		if (id._id > _graph_nodes.size())
+			ErrorOutputter.fatalError("Graph node id (" + id._id + ")not found.\n");
 
-	  return _graph_nodes.get(id._id);
+		return _graph_nodes.get(id._id);
 	}
 }
---
+++ src/gvpl/jdt/GraphBuilder.java
@@ -0,0 +1,214 @@
+package gvpl.jdt;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import gvpl.jdt.Graph.GraphNode;
+import gvpl.jdt.Graph.GraphNodeId;
+import gvpl.jdt.Graph.NodeType;
+
+public class GraphBuilder {
+
+	enum eUnOp {
+		E_INVALID_UN_OP,
+		E_PLUS_PLUS_OP
+	};
+
+	enum eBinOp {
+		E_INVALID_BIN_OP,
+		E_ADD_OP,
+		E_SUB_OP,
+		E_MULT_OP,
+		E_DIV_OP,
+		E_LESS_THAN_OP,
+		E_GREATER_THAN_OP
+	}
+
+	enum eAssignBinOp {
+		E_INVALID_A_BIN_OP,
+		E_ASSIGN_OP,
+		E_PLUS_ASSIGN_OP,
+		E_SUB_ASSIGN_OP
+	}
+
+	enum eValueType {
+		E_INVALID_TYPE,
+		E_INT,
+		E_FLOAT,
+		E_DOUBLE,
+		E_STRING,
+		E_BOOL
+	}
+
+	enum eForLoopState {
+		E_INVALID_LOOP_STATE,
+		E_INIT,
+		E_CONDITION_EXPR,
+		E_POST_EXPR,
+		E_BASIC_BLOCK,
+		E_OUT_OF_LOOP
+	}
+
+	public class VarId {
+
+		private int _id;
+
+		public VarId(int id) {
+			_id = id;
+		}
+	}
+
+	private class VarDecl {
+		private VarId _id;
+		private String _name;
+		private String _type;
+		private GraphNodeId _curr_graph_node_id;
+
+		public VarDecl() {
+			_id = null;
+			_curr_graph_node_id = null;
+		}
+	}
+
+	private Map<eBinOp, String> _bin_op_strings = new EnumMap<eBinOp, String>(eBinOp.class);
+	private Map<eUnOp, String> _un_op_strings = new EnumMap<eUnOp, String>(eUnOp.class);
+	private Map<eAssignBinOp, String> _assign_bin_op_strings = new EnumMap<eAssignBinOp, String>(
+			eAssignBinOp.class);
+
+	/** Stores all the graph */
+	private Graph _gvpl_graph;
+
+	// TODO clear the variables that aren't in scope anymore
+	/** Converts a ast node id to a graph node id */
+	private Map<VarId, VarDecl> _var_graph_nodes;
+
+	private List<Graph> _for_loops = new ArrayList<Graph>();
+
+	private eForLoopState _for_loop_state;
+
+	/** Converts a ast node id to a VarDecl instance */
+	// Map<var_id, VarDecl> _ast_variables;
+
+	public GraphBuilder(Graph gvpl_graph)
+
+	{
+		_gvpl_graph = gvpl_graph;
+		_for_loop_state = eForLoopState.E_OUT_OF_LOOP;
+
+		// _bin_op_strings[E_ASSIGN_OP] = "=";
+
+		_bin_op_strings.put(eBinOp.E_ADD_OP, "+");
+		_bin_op_strings.put(eBinOp.E_SUB_OP, "-");
+		_bin_op_strings.put(eBinOp.E_MULT_OP, "*");
+		_bin_op_strings.put(eBinOp.E_DIV_OP, "/");
+		_bin_op_strings.put(eBinOp.E_LESS_THAN_OP, "<=");
+		_bin_op_strings.put(eBinOp.E_GREATER_THAN_OP, ">=");
+
+		_un_op_strings.put(eUnOp.E_PLUS_PLUS_OP, "++");
+
+		_assign_bin_op_strings.put(eAssignBinOp.E_PLUS_ASSIGN_OP, "+");
+		_assign_bin_op_strings.put(eAssignBinOp.E_SUB_ASSIGN_OP, "-");
+	}
+
+	void add_var_decl(VarDecl var_decl) {
+		_var_graph_nodes.put(var_decl._id, var_decl);
+	}
+
+	GraphNode add_direct_val(eValueType type, String value) {
+		return _gvpl_graph.add_graph_node(value, NodeType.E_DIRECT_VALUE);
+	}
+
+	void add_assign_op(VarId lhs, GraphNode rhs_node) {
+		VarDecl var_decl = find_var(lhs);
+
+		GraphNode lhs_node = _gvpl_graph.add_graph_node(var_decl._name, NodeType.E_VARIABLE);
+		var_decl._curr_graph_node_id = lhs_node._id;
+
+		rhs_node._dependent_nodes.add(lhs_node);
+	}
+
+	GraphNode add_un_op(eUnOp op, GraphNode val_node) {
+		GraphNode un_op_node = _gvpl_graph.add_graph_node(_un_op_strings.get(op),
+				NodeType.E_OPERATION);
+
+		val_node._dependent_nodes.add(un_op_node);
+
+		return un_op_node;
+	}
+
+	GraphNode add_bin_op(eBinOp op, GraphNode val1_node, GraphNode val2_node) {
+		GraphNode bin_op_node = _gvpl_graph.add_graph_node(_bin_op_strings.get(op),
+				NodeType.E_OPERATION);
+
+		val1_node._dependent_nodes.add(bin_op_node);
+		val2_node._dependent_nodes.add(bin_op_node);
+
+		return bin_op_node;
+	}
+
+	GraphNode add_assign_bin_op(eAssignBinOp op, VarId lhs_var_id, GraphNode lhs_node,
+			GraphNode rhs_node) {
+		GraphNode bin_op_node = _gvpl_graph.add_graph_node(_assign_bin_op_strings.get(op),
+				NodeType.E_OPERATION);
+
+		lhs_node._dependent_nodes.add(bin_op_node);
+		rhs_node._dependent_nodes.add(bin_op_node);
+
+		GraphNode result_node = _gvpl_graph.add_graph_node(lhs_node._name, NodeType.E_VARIABLE);
+		bin_op_node._dependent_nodes.add(result_node);
+
+		VarDecl var_decl = find_var(lhs_var_id);
+		var_decl._curr_graph_node_id = result_node._id;
+
+		return result_node;
+	}
+
+	GraphNode add_var_ref(VarId var) {
+		VarDecl var_decl = find_var(var);
+		return _gvpl_graph.find_graph_node(var_decl._curr_graph_node_id);
+	}
+
+	VarDecl find_var(VarId id) {
+		VarDecl temp = _var_graph_nodes.get(id);
+
+		if (temp == null)
+			ErrorOutputter.fatalError("VarId " + id + " not found.\n");
+
+		return temp;
+	}
+
+	void enter_for_init_expr() {
+		if ((_for_loop_state != eForLoopState.E_BASIC_BLOCK)
+				&& (_for_loop_state != eForLoopState.E_OUT_OF_LOOP))
+			ErrorOutputter.fatalError("Invalid state in for loop");
+		_for_loop_state = eForLoopState.E_INIT;
+	}
+
+	void enter_for_condition_expr() {
+		if (_for_loop_state != eForLoopState.E_INIT)
+			ErrorOutputter.fatalError("Invalid state in for loop");
+		_for_loop_state = eForLoopState.E_CONDITION_EXPR;
+	}
+
+	void enter_for_post_expr() {
+		if (_for_loop_state != eForLoopState.E_CONDITION_EXPR)
+			ErrorOutputter.fatalError("Invalid state in for loop");
+		_for_loop_state = eForLoopState.E_POST_EXPR;
+	}
+
+	void enter_for_basic_block() {
+		if (_for_loop_state != eForLoopState.E_POST_EXPR)
+			ErrorOutputter.fatalError("Invalid state in for loop");
+		_for_loop_state = eForLoopState.E_BASIC_BLOCK;
+	}
+
+	void exit_for_loop() {
+		if ((_for_loop_state != eForLoopState.E_BASIC_BLOCK)
+				&& (_for_loop_state != eForLoopState.E_OUT_OF_LOOP))
+			ErrorOutputter.fatalError("Invalid state in for loop");
+		_for_loop_state = eForLoopState.E_OUT_OF_LOOP;
+	}
+
+}
