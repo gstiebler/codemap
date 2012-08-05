e369533c31b56b2453cc6572df8f519483f97d7a
Guilherme Stiebler <guilhermemst@gmail.com>
2012-08-05 16:29:38 -0300

    inclusão de exemplo com subgrafo

---
+++ fixtures/func_call/func_call.cpp
@@ -0,0 +1,15 @@
+
+int soma(int a, int b)
+{
+	int resultado;
+	resultado = a + b;
+	return resultado;
+}
+
+int main() 
+{
+	int x;
+	int a = 3;
+	int b = 5;
+	int c = soma(a, b);
+}
--- src/tests/BasicTest.java
+++ src/tests/BasicTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 import gvpl.cdt.AstInterpreter;
 import gvpl.common.File;
 import gvpl.graph.GraphBuilder;
+import gvpl.graphviz.FileDriver;
+import gvpl.graphviz.Visualizer;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -27,13 +29,16 @@ public class BasicTest {
 	
 	@Test
 	public void outroTeste() {
-		String examplePath = "K:/Projetos/GVPL/src/tests/fixtures/basic/";
-        Graph gvGraph = DotTree.getGraphFromDot(examplePath + "basic.dot");
+		baseTest("K:/Projetos/GVPL/fixtures/", "func_call");
+	}
+	
+	private void baseTest(String fixturesPath, String testName) {
+		String examplePath = fixturesPath + testName + "/";
         
 		IParserLogService log = new DefaultLogService();
 		String code = "";
 		try {
-			code = File.readFileToString(examplePath + "basic.cpp");
+			code = File.readFileToString(examplePath + testName + ".cpp");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -58,6 +63,11 @@ public class BasicTest {
 		GraphBuilder graph_builder = new GraphBuilder();
 		new AstInterpreter(graph_builder, translationUnit);
 		
+		FileDriver fileDriver = new gvpl.graphviz.FileDriver();
+		Visualizer visualizer = new Visualizer(fileDriver);
+		fileDriver.print(graph_builder._gvplGraph, examplePath + "generated.dot", visualizer);
+
+        Graph gvGraph = DotTree.getGraphFromDot(examplePath + testName + ".dot");
 		assertTrue(GraphCompare.isEqual(graph_builder._gvplGraph, gvGraph));
 	}
 	
