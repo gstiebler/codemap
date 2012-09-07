a655698429531a12fbeb9346089a126bea4b8db2
Guilherme Stiebler <guilhermemst@gmail.com>
2012-09-07 16:31:10 -0300

    simplificação no código. Alteração no exemplo de func_pointer_reference

--- fixtures/func_pointer_reference/func_pointer_reference.cpp
+++ fixtures/func_pointer_reference/func_pointer_reference.cpp
@@ -18,11 +18,10 @@ int funcao(UnicaClass &param_ref, UnicaClass *param_pointer)
 int main() 
 {
 	UnicaClass ucRef1(1), ucRef2(2), *ucP1, *ucP2;
-	int a = ucRef1._memberInt;
-	//ucP1 = new UnicaClass(10);
-	//ucP2 = new UnicaClass(20);
+	ucP1 = new UnicaClass(10);
+	ucP2 = new UnicaClass(20);
 	
-	//int natural = funcao(ucRef1, ucP1);
+	int natural = funcao(ucRef1, ucP1);
 	//int refRef = funcao(ucRef1, &ucRef2);
 	//int pointerPointer = funcao(*ucP1, ucP2);
 	//int reverse = funcao(*ucP1, &ucRef2);
