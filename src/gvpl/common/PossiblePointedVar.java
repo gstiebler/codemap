c309289a7d89d7db1c6484a0b1a7d4574f3a5550
Guilherme Stiebler <guilhermemst@gmail.com>
2012-10-03 21:24:00 -0300

    adicionando arquivo

---
+++ src/gvpl/common/PossiblePointedVar.java
@@ -0,0 +1,47 @@
+package gvpl.common;
+
+import gvpl.graph.Graph;
+import gvpl.graph.GraphNode;
+import gvpl.graph.Graph.NodeType;
+
+class PossiblePointedVar {
+	PossiblePointedVar _varTrue = null;
+	PossiblePointedVar _varFalse = null;
+	GraphNode _conditionNode = null;
+	Var _finalVar = null;
+
+	PossiblePointedVar() {
+
+	}
+
+	PossiblePointedVar(Var finalVar) {
+		setVar(finalVar);
+
+	}
+
+	void setVar(Var finalVar) {
+		_finalVar = finalVar;
+		_conditionNode = null;
+	}
+
+	void setPossibleVars(GraphNode conditionNode, PossiblePointedVar varTrue,
+			PossiblePointedVar varFalse) {
+		_conditionNode = conditionNode;
+		_varTrue = varTrue;
+		_varFalse = varFalse;
+		_finalVar = null;
+	}
+	
+	GraphNode getIfNode(Graph graph, int startingLine) {
+		if(_conditionNode == null)
+			return _finalVar.getCurrentNode(startingLine);
+			
+		GraphNode ifOpNode = graph.addGraphNode("If", NodeType.E_OPERATION, startingLine);
+
+		_varTrue.getIfNode(graph, startingLine).addDependentNode(ifOpNode, startingLine);
+		_varFalse.getIfNode(graph, startingLine).addDependentNode(ifOpNode, startingLine);
+		_conditionNode.addDependentNode(ifOpNode, startingLine);
+		
+		return ifOpNode;
+	}
+}
