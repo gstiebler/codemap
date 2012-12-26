4d9fd73c4e8b385b9c83e24730dc195a4a9d1759
Guilherme Stiebler <guilhermemst@gmail.com>
2012-12-26 17:07:26 -0200

    correção no JavaScript

--- fixtures/events/main.js
+++ fixtures/events/main.js
@@ -3,12 +3,13 @@ var globalScriptManager = 0;
 
 function signal_connect(objectPointer, eventStr, func, userData) {
 	out.println('event1 added in javascript');
-	if(eventStr == 'clicked')
-		globalScriptManager.addEventFunc(func, {objectPointer, userData});
+	if(eventStr == 'clicked') {
+		var paramsArray = new Array(objectPointer, userData);
+		globalScriptManager.addEventFunc(func, paramsArray);
+	}
 }
 
 function main(scriptManager) {
 	globalScriptManager = scriptManager;
 	globalScriptManager.addFunction('gtk_signal_connect', signal_connect);
-	return event1;
 }
