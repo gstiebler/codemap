b6cbe7fa3ef07c445b7da4e895b64b58cb0c54ae
Guilherme Stiebler <guilhermemst@gmail.com>
2012-12-28 11:36:11 -0200

    refactoring, código de execução de arquivos que ficava nos testes agora fica na classe principal

--- src/gvpl/cdt/Codemap.java
+++ src/gvpl/cdt/Codemap.java
@@ -1,15 +1,24 @@
 package gvpl.cdt;
 
+import static org.junit.Assert.assertTrue;
+import gvpl.common.CodeLocation;
 import gvpl.common.FileFuncs;
+import gvpl.common.ScriptManager;
 import gvpl.graph.Graph;
+import gvpl.graph.GraphNode;
 import gvpl.graphviz.FileDriver;
 import gvpl.graphviz.Visualizer;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.cesta.parsers.dot.DotTree;
 import org.eclipse.cdt.core.dom.ICodeReaderFactory;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.ast.gnu.cpp.GPPLanguage;
@@ -19,9 +28,16 @@ import org.eclipse.cdt.core.parser.IParserLogService;
 import org.eclipse.cdt.core.parser.IScannerInfo;
 import org.eclipse.cdt.core.parser.ScannerInfo;
 import org.eclipse.cdt.internal.core.parser.scanner2.FileCodeReaderFactory;
+import org.eclipse.core.runtime.CoreException;
+
+import tests.GraphCompare;
+import tests.TestsUtil;
+import debug.DebugOptions;
 
 public class Codemap {
 
+	static Logger logger = LogManager.getLogger(Codemap.class.getName());
+	
 	public static void main(String[] args) throws Exception {
 		IParserLogService log = new DefaultLogService();
 		
@@ -53,5 +69,104 @@ public class Codemap {
 		}
 		
 		fileDriver.print(gvplGraph, outFile, visualizer);
+	}
+	
+	public static AstInterpreterCDT execute(String basePath, String mainFile) {
+		DebugOptions.resetLines();
+		GraphNode.resetCounter();
+		FileDriver.resetCounter();
+
+		IParserLogService log = new DefaultLogService();
+		List<String> readIncludePaths = null;
+		String includesFilePath = basePath + "/includes.txt";
+		try {
+			readIncludePaths = FileFuncs.readLines(new java.io.File(includesFilePath));
+		} catch (Exception e1) {
+			readIncludePaths = null;
+		}
+
+		String[] includePaths = null;
+		if (readIncludePaths == null) {
+			includePaths = new String[0];
+			includePaths[0] = basePath;
+		} else {
+			for (int i = 0; i < readIncludePaths.size(); i++)
+				readIncludePaths.set(i, basePath + readIncludePaths.get(i));
+			includePaths = new String[readIncludePaths.size() + 1];
+			readIncludePaths.add(basePath);
+			readIncludePaths.toArray(includePaths);
+		}
+
+		Map<String, String> definedSymbols = new LinkedHashMap<String, String>();
+		IScannerInfo info = new ScannerInfo(definedSymbols, includePaths);
+		ICodeReaderFactory readerFactory = FileCodeReaderFactory.getInstance();
+
+		AstInterpreterCDT astInterpreter = new AstInterpreterCDT(new gvpl.graph.Graph());
+		ScriptManager sm = new ScriptManager(basePath, astInterpreter);
+		astInterpreter.setScriptManager(sm);
+		sm.execMainScript();
+		
+		List<String> fileNames = new ArrayList<String>();
+		
+		List<String> readBuildFiles = null;
+		try {
+			readBuildFiles = FileFuncs.readLines(new java.io.File(basePath + "files.txt"));
+		} catch (Exception e1) {
+			readBuildFiles = null;
+		}
+		
+		if(readBuildFiles != null){
+			for (String buildFile : readBuildFiles)
+				fileNames.add(basePath + buildFile);
+		}
+		
+		fileNames.add(mainFile);
+		
+		List<IASTTranslationUnit> translationUnits = new ArrayList<IASTTranslationUnit>();
+		
+		for(String fileName : fileNames)
+		{
+			logger.debug(" -- ** Processing cpp: {}", fileName);
+			CodeLocation.setCurrentFileName(fileName);
+			String code = "";
+			try {
+				code = FileFuncs.readFileToString(fileName);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			CodeReader reader = new CodeReader(code.toCharArray());
+			IASTTranslationUnit translationUnit = null;
+			try {
+				translationUnit = GPPLanguage.getDefault().getASTTranslationUnit(reader, info,
+						readerFactory, null, log);
+				translationUnits.add(translationUnit);
+			} catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for(int i = 0; i < translationUnits.size(); ++i) {
+			DebugOptions.setCurrCpp(fileNames.get(i));
+			logger.debug(" -*- Loading declarations {}", fileNames.get(i));
+			astInterpreter.loadDeclarations(translationUnits.get(i));
+		}
+		
+		astInterpreter.loadDefinitions();
+
+		FileDriver fileDriver = new gvpl.graphviz.FileDriver();
+		Visualizer visualizer = new Visualizer(fileDriver);
+
+		FileWriter outFile = null;
+		try {
+			outFile = new FileWriter(basePath + "generated.dot");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		fileDriver.print(astInterpreter.getGraph(), outFile, visualizer);
+		return astInterpreter;
 	}
 }
--- src/tests/TestsUtil.java
+++ src/tests/TestsUtil.java
@@ -2,142 +2,24 @@ package tests;
 
 import static org.junit.Assert.assertTrue;
 import gvpl.cdt.AstInterpreterCDT;
-import gvpl.common.CodeLocation;
-import gvpl.common.FileFuncs;
-import gvpl.common.ScriptManager;
-import gvpl.graph.GraphNode;
-import gvpl.graphviz.FileDriver;
-import gvpl.graphviz.Visualizer;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import gvpl.cdt.Codemap;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.cesta.parsers.dot.DotTree;
 import org.cesta.parsers.dot.DotTree.Graph;
-import org.eclipse.cdt.core.dom.ICodeReaderFactory;
-import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
-import org.eclipse.cdt.core.dom.ast.gnu.cpp.GPPLanguage;
-import org.eclipse.cdt.core.parser.CodeReader;
-import org.eclipse.cdt.core.parser.DefaultLogService;
-import org.eclipse.cdt.core.parser.IParserLogService;
-import org.eclipse.cdt.core.parser.IScannerInfo;
-import org.eclipse.cdt.core.parser.ScannerInfo;
-import org.eclipse.cdt.internal.core.parser.scanner2.FileCodeReaderFactory;
-import org.eclipse.core.runtime.CoreException;
-
-import debug.DebugOptions;
 
 public class TestsUtil {
 	
 	static Logger logger = LogManager.getLogger(TestsUtil.class.getName());
 	
 	public static void baseTest(String testName) {
-		DebugOptions.resetLines();
 		String fixturesPath = System.getProperty("user.dir") + "/fixtures/";
 		String examplePath = fixturesPath + testName + "/";
-		GraphNode.resetCounter();
-		FileDriver.resetCounter();
-
-		IParserLogService log = new DefaultLogService();
-
-		Map<String, String> definedSymbols = new LinkedHashMap<String, String>();
-
-		List<String> readIncludePaths = null;
-		try {
-			readIncludePaths = FileFuncs.readLines(new java.io.File(examplePath + "includes.txt"));
-		} catch (Exception e1) {
-			readIncludePaths = null;
-		}
-
-		String[] includePaths = null;
-		if (readIncludePaths == null) {
-			includePaths = new String[0];
-			includePaths[0] = examplePath;
-		} else {
-			for (int i = 0; i < readIncludePaths.size(); i++)
-				readIncludePaths.set(i, examplePath + readIncludePaths.get(i));
-			includePaths = new String[readIncludePaths.size() + 1];
-			readIncludePaths.add(examplePath);
-			readIncludePaths.toArray(includePaths);
-		}
-
-		IScannerInfo info = new ScannerInfo(definedSymbols, includePaths);
-		ICodeReaderFactory readerFactory = FileCodeReaderFactory.getInstance();
-
-		AstInterpreterCDT astInterpreter = new AstInterpreterCDT(new gvpl.graph.Graph());
-		ScriptManager sm = new ScriptManager(examplePath, astInterpreter);
-		astInterpreter.setScriptManager(sm);
-		sm.execMainScript();
+		String mainFile = examplePath + testName + ".cpp";
 		
-		List<String> fileNames = new ArrayList<String>();
-		String mainTestFileName = examplePath + testName + ".cpp";
+		AstInterpreterCDT astInterpreter = Codemap.execute(examplePath, mainFile);
 		
-		List<String> readBuildFiles = null;
-		try {
-			readBuildFiles = FileFuncs.readLines(new java.io.File(examplePath + "files.txt"));
-		} catch (Exception e1) {
-			readBuildFiles = null;
-		}
-		
-		if(readBuildFiles != null){
-			for (String buildFile : readBuildFiles)
-				fileNames.add(examplePath + buildFile);
-		}
-		
-		fileNames.add(mainTestFileName);
-		
-		List<IASTTranslationUnit> translationUnits = new ArrayList<IASTTranslationUnit>();
-		
-		for(String fileName : fileNames)
-		{
-			logger.debug(" -- ** Processing cpp: {}", fileName);
-			CodeLocation.setCurrentFileName(fileName);
-			String code = "";
-			try {
-				code = FileFuncs.readFileToString(fileName);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			CodeReader reader = new CodeReader(code.toCharArray());
-			IASTTranslationUnit translationUnit = null;
-			try {
-				translationUnit = GPPLanguage.getDefault().getASTTranslationUnit(reader, info,
-						readerFactory, null, log);
-				translationUnits.add(translationUnit);
-			} catch (CoreException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		for(int i = 0; i < translationUnits.size(); ++i) {
-			DebugOptions.setCurrCpp(fileNames.get(i));
-			logger.debug(" -*- Loading declarations {}", fileNames.get(i));
-			astInterpreter.loadDeclarations(translationUnits.get(i));
-		}
-		
-		astInterpreter.loadDefinitions();
-
-		FileDriver fileDriver = new gvpl.graphviz.FileDriver();
-		Visualizer visualizer = new Visualizer(fileDriver);
-
-		FileWriter outFile = null;
-		try {
-			outFile = new FileWriter(examplePath + "generated.dot");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		fileDriver.print(astInterpreter.getGraph(), outFile, visualizer);
-
 		Graph gvGraph = DotTree.getGraphFromDot(examplePath + testName + ".dot");
 		assertTrue(GraphCompare.isEqual(astInterpreter.getGraph(), gvGraph));
 	}
