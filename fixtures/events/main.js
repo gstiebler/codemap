e0f240ad9a3b54b6c29da6d5c2bd563ff970ef83
Guilherme Stiebler <guilhermemst@gmail.com>
2012-12-28 11:12:22 -0200

    remoção de impressão no javascript

--- fixtures/events/main.js
+++ fixtures/events/main.js
@@ -2,8 +2,6 @@
 var globalScriptManager = 0;
 
 function signal_connect(objectPointer, eventStr, func, userData) {
-	out.println('event1 added in javascript');
-	out.println(eventStr.getNode().getName());
 	if(eventStr.getNode().getName() == '"clicked"') {
 		var paramsArray = new Array(objectPointer, userData);
 		globalScriptManager.addEventFunc(func, paramsArray);
