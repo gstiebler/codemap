ed0d242dca2692adda958c9b7373b6d7d2699c56
Guilherme Stiebler <guilhermemst@gmail.com>
2013-04-28 19:54:46 -0300

    atualização do exemplo

--- fixtures/deep_method/deep_method.cpp
+++ fixtures/deep_method/deep_method.cpp
@@ -4,29 +4,51 @@ class cClass
 public:
 	float a;
 	float b;
+    float c;
 	
-    void calledFunc()
+    void calledFuncA()
     {
-        a = 8;
+        a = 18;
+    }
+   
+    void calledFuncB()
+    {
+        b = 28;
+    }
+	
+    void calledFuncC()
+    {
+        c = 38;
     }
     
 	void func1()
     {
-        int c = 3;
+        int K = 3;
+        bool cond = K > 4;
         
-        if( c > 4 )
-            calledFunc();
-        //for( int i(0); i < c; ++i)
-        //{
-        //    if( i > 2 )
-        //        calledFunc();
-        //}
+        if( cond )
+            calledFuncA();
+               
+        {
+            if( cond )
+                calledFuncB();
+        }
+            
+        for( int i(0); i < K; ++i)
+        {
+            if( i > 2 )
+                calledFuncC();
+        }
     }
 };
 
 int main() {
     cClass instance;
-    instance.a = 7;
+    instance.a = 17;
+    instance.b = 27;
+    instance.c = 37;
     instance.func1();
     float x = instance.a;
+    float y = instance.b;
+    float z = instance.c;
 }
